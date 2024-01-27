e4aa4a93b9c899526ea0479d550d244ecad64638
Carlos Ramírez <rrscarlos@uma.es>
2024-01-27 11:16:00 +0100

    add dataset

--- run_chest.sh
+++ run_chest.sh
@@ -3,4 +3,4 @@
 # Remove CD: 84.57
 # Remove flip: 81.02
 # Original resize: 80.04
-CUDA_VISIBLE_DEVICES=1 python3 train_network_dbt.py --dataset_path /data/usr/hd108/chest_xray_dataset --category chest --patch_size 128 --batch_size 300
+CUDA_VISIBLE_DEVICES=1 python3 train_network_dbt.py --dataset_path /data/chest_xray --category chest --patch_size 128 --batch_size 300
